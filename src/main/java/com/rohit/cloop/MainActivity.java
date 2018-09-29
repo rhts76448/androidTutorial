ce4589453e16b3e69ac42e0f1f962c0cb5f86c3c
rhts76448 <rohitpatel76448@gmail.com>
2018-09-29 17:56:56 +0530

    completion of assign

--- src/main/java/com/rohit/cloop/MainActivity.java
+++ src/main/java/com/rohit/cloop/MainActivity.java
@@ -2,6 +2,7 @@ package com.rohit.cloop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText et = (EditText) findViewById(R.id.countryCode);
+        et.setText(new String(Character.toChars(uDDEE))+ "+91");
     }
+
+
 }
