30bcbddd520081b4c33d91c76d9dca0699050cd8
rhts76448 <rohitpatel76448@gmail.com>
2018-10-02 12:14:58 +0530

    adding ind flag

--- src/main/java/com/rohit/cloop/MainActivity.java
+++ src/main/java/com/rohit/cloop/MainActivity.java
@@ -1,7 +1,10 @@
 package com.rohit.cloop;
 
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.WindowManager;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,7 +15,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         EditText et = (EditText) findViewById(R.id.countryCode);
-        et.setText(new String(Character.toChars(uDDEE))+ "+91");
+        et.setText(new String("\ud83c\uddee\ud83c\uddf3")+ "+91");
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        adjustFontScale(new Configuration(newConfig));
+    }
+
+    public void adjustFontScale(Configuration configuration) {
+        if (configuration != null && configuration.fontScale != 1.0) {
+            configuration.fontScale = (float) 1.0;
+            DisplayMetrics metrics = getResources().getDisplayMetrics();
+            WindowManager wm = (WindowManager) getSystemService(WINDOW_SERVICE);
+            wm.getDefaultDisplay().getMetrics(metrics);
+            metrics.scaledDensity = configuration.fontScale * metrics.density;
+            getBaseContext().getResources().updateConfiguration(configuration, metrics);
+        }
     }
 
 
